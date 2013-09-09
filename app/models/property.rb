4666b85c2d4c194be0ffc3064d22c0d41e852f74
Edu Erondu <eduerondu@gmail.com>
2013-09-08 23:15:14 -0500

    initialize the Property object with a zillow property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,21 @@
 class Property < ActiveRecord::Base
+  attr_reader :property, :zillowId
+  has_many :holdDeals
+  def initialize(attributes = {})
+    super
+    puts attributes[:address].nil?
+
+    if attributes[:address].nil?
+      raise ArgumentError, "The address options is required" 
+    end
+
+    if attributes[:zipcode].nil?
+      raise ArgumentError, "The zipcode options is required" 
+    end
+    p @property
+
+    @property = Rubillow::PropertyDetails.deep_search_results address: attributes[:address], citystatezip: attributes[:zipcode]
+    @address = attributes["address"] if attributes.present? && !attributes["city"].present?
+    @zillowId = @property.zpid if @property
+  end
 end
