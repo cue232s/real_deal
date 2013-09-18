c718beaa6b02f978e405a2881ce55409bd869cc2
Edu Erondu <eduerondu@gmail.com>
2013-09-18 07:52:45 -0500

    working on devising hold deal structure

--- app/models/hold_deal.rb
+++ app/models/hold_deal.rb
@@ -1,2 +1,12 @@
 class HoldDeal < ActiveRecord::Base
+  belongs_to :property, dependent:
+
+  def total_investment 
+    @total_investment = self.asking_price - (self.asking_price * self.asking_price_discount)
+    @total_investment += self.rehab_cost
+  end
+
+  def caluculate_arv
+    
+  end
 end
--- db/migrate/20130917025548_create_hold_deals.rb
+++ db/migrate/20130917025548_create_hold_deals.rb
@@ -2,18 +2,31 @@ class CreateHoldDeals < ActiveRecord::Migration
   def change
     create_table :hold_deals do |t|
       t.property_id :integer
-      t.name :string
-      t.asking_price : :decimal, :precision => 8, :scale => 2
-      t.price_per_sq_ft :decimal, :precision => 8, :scale => 2
-      t.asking_price_discount :float
-      t.rehab_cost_per_sq_ft ::decimal, :precision => 8, :scale => 2
-      t.gross_total :decimal, :precision => 8, :scale => 2
-      t.marketing_expense :decimal, :precision => 8, :scale => 2
-      t.re_commision :decimal, :precision => 8, :scale => 2
+
+      #Investment (purchase/rehab)
+      #p1
+      #t.total_investment 
+        #s1
+        t.asking_price : :decimal, :precision => 8, :scale => 2
+        t.aslking_price_per_sq_ft :decimal, :precision => 8, :scale => 2
+        t.asking_price_discount :float
+        # @rehab_cost
+        t.rehab_cost_per_sq_ft :decimal, :precision => 8, :scale => 2
+
+
+      #Property Value / Post Rehab
+      t.est_arv :decimal, :precision => 8, :scale => 2
+        t.comp_avg_per_sq_ft :decimal, :precision => 8, :scale => 2
+        #t.property.sqft
+
+      #Exit
+      #t.cash_out
+        t.arv_per_sq_ft :decimal, :precision => 8, :scale => 2
+        t.gross_total :decimal, :precision => 8, :scale => 2
+        t.marketing_expense :decimal, :precision => 8, :scale => 2
+        t.re_commision :decimal, :precision => 8, :scale => 2
       t.cost_of_capital :decimal, :precision => 8, :scale => 2
 
-      t.estimated_arv :decimal, :precision => 8, :scale => 2
-      t.estimated_arv_per_sq_ft :decimal, :precision => 8, :scale => 2
       t.exit_percentage_of_arv :decimal, :precision => 8, :scale => 2
       t.exit_percentage_of_arv_per_sq_ft :decimal, :precision => 8, :scale => 2
       t.exit_sale_price :decimal, :precision => 8, :scale => 2
